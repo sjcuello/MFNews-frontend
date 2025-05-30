ce8fd65527d19b13db11b929f216c7912d4ec70f
Santiago Cuello <sjcuello@gmail.com>
2025-05-30 16:50:45 -0300

    Some styling

--- src/components/news/sideArticle/index.tsx
+++ src/components/news/sideArticle/index.tsx
@@ -2,13 +2,28 @@ import { Box, Typography } from "@mui/material";
 import { Article } from "../../../interfaces";
 import ImageContainer from "../imageContainer";
 import styles from "./styles.module.css";
+import { useMemo } from "react";
 
-const SideArticle = ({ title, imageUrl}: Article) => {
+
+const getRandomPastelColor = () => {
+  const hue = Math.floor(Math.random() * 360);
+  return `hsl(${hue}, 40%, 90%)`;
+};
+
+const SideArticle = ({ title, category, imageUrl }: Article) => {
+  const backgroundColor = useMemo(getRandomPastelColor, []);
+
   return (
-    <Box className={styles.sideArticle}>
+    <Box className={styles.sideArticle} style={{ backgroundColor }}>
       <Box className={styles.sideArticleContent}>
-      <Typography variant='h3'>TODO: category</Typography>
-      <Typography variant='h2'>{title}</Typography>
+      <Typography
+        variant='h5'
+        fontWeight={800}
+        className={styles.category}
+      >
+        {category}
+      </Typography>
+      <Typography variant='h4'>{title}</Typography>
       </Box>
       <Box className={styles.sideArticleImage}>
         <ImageContainer imageUrl={imageUrl} title={title} />
